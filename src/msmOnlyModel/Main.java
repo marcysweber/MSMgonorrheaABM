1f0f9c3a680b8496972d1a6579bc7f3e597ba63c
marcysweber <marcysweber@gmail.com>
2026-05-08 15:32:14 -0400

    uniqueID

--- src/msmOnlyModel/Main.java
+++ src/msmOnlyModel/Main.java
@@ -81,7 +81,7 @@ public class Main {
 				
 				//executeCompareResistanceInserters(scenariofile);
 //				
-				//executeFitnessCostSA(scenariofile);
+				executeFitnessCostSA(scenariofile);
 			}
 			
 			
